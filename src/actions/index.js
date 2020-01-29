47e47389c9f687aeac572085cd04912c7cf04a7e
bright okike <brightokike@gmail.com>
2020-01-29 21:59:53 +0300

    implemented the edit streamcomponent and some css styling

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,6 @@
 import streams from '../apis/streams';
+import history from '../history';
+
 import { 
 	SIGN_IN, 
 	SIGN_OUT, 
@@ -27,6 +29,7 @@ export const createStream = formValues => async (dispatch, getState) => {
 	const response = await streams.post('/streams', { ...formValues, userId });
 
 	dispatch({ type: CREATE_STREAM, payload: response.data })
+	history.push('/');
 };
 
 export const fetchStreams = () => async dispatch => {
@@ -42,9 +45,10 @@ export const fetchStream = (id) => async dispatch => {
 };
 
 export const editStream = (id, formValues) => async dispatch => {
-	const response = await streams.put(`/streams/${id}`, formValues);
+	const response = await streams.patch(`/streams/${id}`, formValues);
 
 	dispatch({ type: EDIT_STREAM, payload: response.data })
+	history.push('/');
 };
 
 export const deleteStream = (id) => async dispatch => {
